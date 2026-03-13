4f20a6ed698b01685ce01937a07c0a54a73ea3df
ShrifGamal <154748642+ShrifGamal@users.noreply.github.com>
2026-03-13 03:07:39 +0200

    Add files via upload

---
+++ Talabat.Repository/Data/Configrations/ProductTypeConfigrations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Data.Configrations
+{
+    public class ProductTypeConfigrations : IEntityTypeConfiguration<ProductType>
+    {
+        public void Configure(EntityTypeBuilder<ProductType> builder)
+        {
+            builder.Property(P => P.Name).IsRequired();
+        }
+    }
+}
---
+++ Talabat.Repository/Identity/Contexts/StoreIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Store.Core.Entites.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Identity.Contexts
+{
+    public class StoreIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public StoreIdentityDbContext(DbContextOptions<StoreIdentityDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
